dcd74ac86f9e29286255c7075fc57cf43448f9d2
Ainaa Azman <ainaasakinah@gmail.com>
2019-03-14 00:14:42 +0900

    add: exercise 4

--- variables/name.rb
+++ variables/name.rb
@@ -1,11 +1,19 @@
 # Variables: Exercise 1
 
-puts "What is your name?"
-name = gets.chomp
-puts "Hello there #{name}"
+# puts "What is your name?"
+# name = gets.chomp
+# puts "Hello there #{name}"
 
 # Variables:Exercise 3
 
-10.times do
-  puts name
-end
+# 10.times do
+#   puts name
+# end
+
+# Variables: Exercise 4
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hi there " + first_name + " " + last_name
