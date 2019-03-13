b70a3419a3b350c43411af88340eed6d9eb89883
Ainaa Azman <ainaasakinah@gmail.com>
2019-03-14 00:01:47 +0900

    fix: restructure exercise format

--- thebasics/thebasics.rb
+++ thebasics/thebasics.rb
@@ -1,16 +1,15 @@
-## Exercise ##
+# The basics: Exercise 1
 
-# 1
 puts "Ainaa " + "Sakinah"
 
-# 2
+# The basics: Exercise 2
 
 puts thousands = 5421 / 1000
 puts hundreds =  5421 % 1000 / 100
 puts tens =  5421 % 100 / 10
 puts ones = 5421 % 100 % 10
 
-# 3
+# The basics: Exercise 3
 
 movies = {
   :alita => 2019,
@@ -24,7 +23,7 @@ puts movies[:avatar]
 puts movies[:toy_story]
 puts movies[:sweeney_todd]
 
-# 4
+# The basics: Exercise 4
 
 dates = [2019, 2009, 1995, 2007]
 
@@ -33,13 +32,13 @@ puts dates[1]
 puts dates[2]
 puts dates[3]
 
-# 5
+# The basics: Exercise 5
 
 puts 5 * 4 * 3 * 2 * 1
 puts 6 * 5 * 4 * 3 * 2 * 1
 puts 7 * 6 * 5 * 4 * 3 * 2 * 1
 puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
 
-# 7
+# The basics: Exercise 7
 
 puts "There is a missing curly bracket "
--- variables/name.rb
+++ variables/name.rb
@@ -1,3 +1,5 @@
+# Variables: Exercise 1
+
 puts "What is your name?"
 name = gets.chomp
 puts "Hello there #{name}"
