02378bb00148f9548554100b5326ef8849fd0687
Ainaa Azman <ainaasakinah@gmail.com>
2019-03-16 13:12:58 +0900

    new: exercise 2 and 3

---
+++ methods/exercise2.rb
@@ -0,0 +1,16 @@
+#  What do the following expressions evaluate to?
+
+1. x = 2
+# Answer = 2
+
+2. puts x = 2
+# Answer = nil
+
+3. p name = "Joe"
+# Answer = "Joe"
+
+4. four = "four"
+# Answer = "four"
+
+5. print something = "nothing"
+# Answer = nil
