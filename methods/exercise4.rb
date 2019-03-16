7cc796c7dda4f3af5f4ab0e7c4cc5807337d1d03
Ainaa Azman <ainaasakinah@gmail.com>
2019-03-16 13:16:42 +0900

    new: methods exercise 4

---
+++ methods/exercise4.rb
@@ -0,0 +1,11 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+
+# Answer = It will not return anything to the screen. Because the code is exited
+# through return in line 3 which returns nothing which is nil in ruby
