e86cf41777056ade219cc5396d1351072a831e51
Ainaa Azman <ainaasakinah@gmail.com>
2019-03-17 19:48:04 +0900

    new: controle flow exercise 6

---
+++ flow_control/exercise6.rb
@@ -0,0 +1,23 @@
+# def equal_to_four(x)
+#   if x == 4
+#     puts "yup"
+#   else
+#     puts "nope"
+# end
+
+# equal_to_four(5)
+
+# Answer = There is no end to close the equal_to_four method.
+# To fix it, indent end on line 6 to close the conditional statement
+# and add another end on line 7 to close the equal_to_four method
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(4)
+equal_to_four(5)
