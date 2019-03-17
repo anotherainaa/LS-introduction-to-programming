8ce2a1cb0be962417bba8d2d67c351144fb60702
Ainaa Azman <ainaasakinah@gmail.com>
2019-03-17 19:28:43 +0900

    change: add condition for negative number and string to include the number input

--- flow_control/exercise3.rb
+++ flow_control/exercise3.rb
@@ -1,14 +1,13 @@
 puts "Insert a number between 0 - 100"
 number = gets.chomp.to_i
 
-def output(number)
-  if number <= 50
-    puts "Number is between 0 to 50"
-  elsif number <= 100
-    puts "Number is between 51 to 100"
-  else
-    puts "Above 100"
-  end
+if number < 0
+  puts "Number is smaller than 0"
+elsif number <= 50
+  puts "#{number} is between 0 to 50"
+elsif number <= 100
+  puts "#{number} is between 51 to 100"
+else
+  puts "#{number} is above 100"
 end
 
-puts output(number)
